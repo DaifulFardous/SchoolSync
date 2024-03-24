3fd89170716e07e51936381628fd414ab9fd3156
sHovonIslamx23 <saifulislamshovon056@gmail.com>
2024-03-24 11:06:31 -0700

    Profile Page Update

--- Frontend/school-sync/src/components/Profile/Profile.jsx
+++ Frontend/school-sync/src/components/Profile/Profile.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import React from 'react';
 import Sidenav from './../SideNav/Sidenav';
-import Uppernav from './../Courses/Uppernav';
+import Uppernav from './../Profile/Uppernav';
+import profilePicture from '../../assets/images/pp.png';
+import { FaPlus } from 'react-icons/fa';
 
-function Dashboard() {
+function Profile() {
   return (
-    <div className='bg-opacity-100 bg-gray-200 min-h-screen'>
-        <div className='flex'>
+    <div className='bg-gray-200 min-h-screen'>
+      <div className='flex'>
         <div className='fixed w-64'>
-            <Sidenav/>
+          <Sidenav />
         </div>
-        <div className='p-6 pl-72 fixed'>
-            <Uppernav/>
+        <div className='flex flex-col ml-64'>
+          <div className='p-6'>
+            <Uppernav />
+          </div>
+          <div className='flex flex-col items-start pt-2 pl-6 gap-y-5'>
+            <div className='w-2/6 rounded-lg bg-white pl-3 flex flex-col items-center pb-12'>
+              <h4 className='text-black pt-3 text-center'>Personal Account</h4>
+              <div className='mt-3 flex items-center justify-center'>
+                <img
+                  src={profilePicture}
+                  alt="Profile Picture"
+                  className="rounded-full h-32 w-32"
+                />
+              </div>
+              <h2 className='text-black pt-2 text-center'>Shovon Islam</h2>
+              <button className='bg-white px-2 py-2 border-2 border-blue-700 text-blue-500 font-semibold rounded-md w-3/5 h-1/5 cursor-pointer hover:bg-blue-50'>
+                Change personal details
+              </button>
+            </div>
+            <div className='w-2/6 rounded-lg bg-white pl-3 flex flex-col pb-12'>
+              <h4 className='text-black pt-3 pl-4 text-start'>Additional Info</h4>
+              <p className='w-4/5 pl-4 font-thin text-gray-500 font-poppins'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit pariatur cum ipsa. Dolorem minus esse corrupti quasi. Fugiat possimus laudantium id?
+              </p>
+              <button className='bg-white px-2 py-2 ml-4 border-2 border-blue-700 text-blue-500 font-semibold rounded-md w-3/5 h-1/5 cursor-pointer hover:bg-blue-50 flex'>
+                <FaPlus className='mr-2' /> Add Additional Info
+              </button>
+            </div>
+          </div>
         </div>
-
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Profile;
