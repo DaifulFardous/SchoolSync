5489424e9cf3e13bcc3e5b06e983e6abb6ff09ad
FARDIN98 <fardinahmed66@gmail.com>
2024-05-21 19:46:04 +0600

    sidenav fixed

--- Frontend/school-sync/src/components/SideNav/Sidenav.jsx
+++ Frontend/school-sync/src/components/SideNav/Sidenav.jsx
@@ -43,35 +43,37 @@ export default function Sidenav() {
 
   return (
     <aside
-      className={`w-[20%] bg-white py-5 px-10 flex h-screen ${
-        isExpanded ? "" : "w-1"
+      className={`bg-white  py-5 px-5 sm:px-10 flex justify-center h-screen shadow-xl ${
+        isExpanded ? " w-1 sm:w-[20%]" : "w-1"
       }`}
     >
       <nav className="flex flex-col flex-1">
-        <div className="flex sm:justify-between justify-center">
+        <div
+          className={`flex justify-center ${
+            isExpanded ? "sm:justify-between" : "sm:justify-center"
+          }`}
+        >
           <div className={`${isExpanded ? "hidden sm:block" : "hidden"}`}>
             <Logo />
           </div>
           <button
             onClick={toggleSidebar}
-            className={`text-2xl ${isExpanded ? "" : "w-min"}`}
+            className={`text-xl ${isExpanded ? "" : ""}`}
           >
             <HiMenu />
           </button>
         </div>
-        <div
-          className={`flex flex-col h-[70%] justify-around ${
-            isExpanded ? "" : "hidden"
-          }`}
-        >
+        <div className={`flex flex-col h-[70%] justify-around`}>
           {navItems.map((item, index) => (
             <a
               href={item.href}
               key={index}
-              className="flex gap-5 text-2xl py-2 sm:px-5 items-center justify-center sm:justify-start hover:bg-gray-200 text-[#6956E5]"
+              className="flex gap-5 text-2xl py-2 sm:px-5 items-center sm:justify-start hover:bg-gray-200 text-[#6956E5]"
             >
               {item.icon}
-              <span className="hidden sm:block">{item.label}</span>
+              <span className={`${isExpanded ? "hidden sm:block" : "hidden"}`}>
+                {item.label}
+              </span>
             </a>
           ))}
         </div>
