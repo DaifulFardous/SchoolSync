0c861a400c618cc76931ff2cec46b33c11b92d96
Rabib <rabibhaque200@gmail.com>
2024-05-26 03:31:52 +0600

    data parsed

--- Frontend/school-sync/src/components/common/Profile.jsx
+++ Frontend/school-sync/src/components/common/Profile.jsx
@@ -1,21 +1,68 @@
-import React, { useContext, useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { FaChevronDown } from "react-icons/fa";
-import { AuthContext } from "../../authContext/authContext";
 
 const Profile = () => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const { imageUrl } = useContext(AuthContext);
-  if (imageUrl) {
-    console.log("Hello", imageUrl);
-  }
+  const [userData, setUserData] = useState("");
+  const token = localStorage.getItem("token");
+
+  //   useEffect( async ()  =>{
+  //     try {
+  //       const response = await axios.post(
+  //         "http://127.0.0.1:8000/api/user/details",
+  //         {
+  //           headers: {
+  //             "Content-Type": "multipart/form-data",
+  //             Authorization: `Bearer ${token}`,
+  //           },
+  //         });
+
+  //   })
+  // }
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
+  const fetchUserDetails = async () => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/api/user/details",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        console.log("Fethced Students:", response.data);
+        setUserData(response.data);
+        console.log(userData.data.image);
+      }
+    } catch (error) {
+      console.error("Error creating course:", error);
+
+      if (error.response && error.response.status === 401) {
+        setError("Unauthorized. Please log in again.");
+        signOut();
+      } else {
+        setError("An error occurred. Please try again.");
+      }
+    }
+  };
 
   return (
     <div className="sm:flex hidden gap-5 items-center">
       <div className="flex items-center gap-5">
-        <img src={imageUrl} alt="Profile" className="rounded-full h-16 w-16" />
+        <img
+          src={userData.data.image}
+          alt="Profile"
+          className="rounded-full h-16 w-16"
+        />
         <div>
-          <div className="font-bold">dummy name</div>
-          <div className="text-sm text-gray-500">dummy profession</div>
+          <div className="font-bold">{userData.data.name}</div>
+          <div className="text-sm text-gray-500">{userData.data.email}</div>
         </div>
       </div>
       <div className="expanded">
